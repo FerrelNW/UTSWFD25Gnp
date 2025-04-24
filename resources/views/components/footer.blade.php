9a313b1797407f9a2436a15bed3f620975370812
FerrelNW <ferrel.nw@gmail.com>
2025-04-24 15:57:10 +0700

    fixed bottom

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="text-center text-sm text-white bg-gray-800 mt-6 py-4 border-t border-gray-300">
+<footer class="fixed bottom-0 left-0 w-full text-center text-sm text-white bg-gray-800 py-4 border-t border-gray-300">
     <p><em>UTS WFD 2025</em></p>
 </footer>
